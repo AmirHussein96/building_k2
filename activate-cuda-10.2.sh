5499a846ad70f0c7fc7d62bfb8c7f9ad2e4dafc6
Amir Hussein <36240131+AmirHussein96@users.noreply.github.com>
2022-06-26 19:05:31 -0400

    Add files via upload

---
+++ activate-cuda-10.2.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export CUDA_HOME=/cm/shared/apps/cudnn8.0-cuda10.2/10.2.89 #/path/to/your/cuda-10.2
+export PATH=$CUDA_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$CUDA_HOME/lib:$LD_LIBRARY_PATH
+
+export CUDA_TOOLKIT_ROOT_DIR=$CUDA_HOME
+export CUDA_TOOLKIT_ROOT=$CUDA_HOME
+export CUDA_BIN_PATH=$CUDA_HOME
+export CUDA_PATH=$CUDA_HOME
+export CUDA_INC_PATH=$CUDA_HOME/include
